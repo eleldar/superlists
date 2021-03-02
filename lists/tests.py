8a22e23d43adee12debfa260415df62829612a48
eldar <eleldar@mail.ru>
2021-03-02 10:06:07 +0300

    module test with html tools from Django

--- lists/tests.py
+++ lists/tests.py
@@ -1,17 +1,15 @@
+from django.template.loader import render_to_string
 from django.test import TestCase
 from django.urls import resolve
 from django.http import HttpRequest
-
-from .views import home_page # 2; функция представления, которую мы собираемся написать далее, и она вернет фактический HTML, который
-                             # нам нужен. Из оператора import вы видите, что мы планируем со-
-                             # хранить ее в lists/views.py.
+from .views import home_page
 
 class HomePageTest(TestCase):
     '''тест домашней страницы'''
 
     def test_root_url_resolves_to_home_page_view(self):
         '''тест: корневой url преобразуется в представление домашней страницы'''
-        found = resolve('/') # 1; функция, которую Django использует внутренне для
+        found = resolve('/') # функция, которую Django использует внутренне для
                              # преобразования URL-адреса и нахождения функций представления,
                              # в соответствие которым они должны быть поставлены
         self.assertEqual(found.func, home_page) # проверяем, чтобы resolve , когда ее вызывают с «/», то есть корнем сайта,
@@ -19,10 +17,9 @@ class HomePageTest(TestCase):
 
     def test_home_page_returns_correct_html(self):
         '''домашняя страница возвращает правильный html'''
-        request = HttpRequest()       # 1; бъект HttpRequest , то есть то, что Django увидит, когда браузер пользователя запросит страницу
-        response = home_page(request) # 2; передаем объект HttpRequest представлению home_page , которое дает отклик
-        html = response.content.decode('utf8') # 3; извлекаем содержимое .content отклика
-        self.assertTrue(html.startswith('<html>')) # 4; нужно, чтобы она начиналась с тега <html> , который закрывается в конце 
-        self.assertIn('<title>To-Do lists</title>', html) # 5; хотим разместить тег <title> со словами «to-do lists» где-нибудь в середине
-        self.assertTrue(html.endswith('</html>')) # 4; закрытие тега в конце
+        response = self.client.get('/') # 1; используется вместо создания объекта HttpRequest вручную и прямого вызова функции представления; используется URL-адрес, который хотим протестировать 
+        html = response.content.decode('utf8') # извлекаем содержимое .content отклика
+        expected_html = render_to_string('lists/home.html') # преобразует шаблон в HTML-разметку
+        self.assertEqual(html, expected_html) # сравнение с тем, что возвращает представление
 
+        self.assertTemplateUsed(response, 'lists/home.html') # 3; позволяет проверить, какой шаблон использовался для вывода отклика как HTML (работает только для откликов, которые были получены тестовым клиентом).

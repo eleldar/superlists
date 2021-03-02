ed8cb2353813f42759c3c03fdb282b49cb81ad2e
eldar <eleldar@mail.ru>
2021-03-02 10:47:33 +0300

    changes of module test

--- lists/tests.py
+++ lists/tests.py
@@ -7,19 +7,7 @@ from .views import home_page
 class HomePageTest(TestCase):
     '''тест домашней страницы'''
 
-    def test_root_url_resolves_to_home_page_view(self):
-        '''тест: корневой url преобразуется в представление домашней страницы'''
-        found = resolve('/') # функция, которую Django использует внутренне для
-                             # преобразования URL-адреса и нахождения функций представления,
-                             # в соответствие которым они должны быть поставлены
-        self.assertEqual(found.func, home_page) # проверяем, чтобы resolve , когда ее вызывают с «/», то есть корнем сайта,
-                                                # нашла функцию под названием home_page .
-
-    def test_home_page_returns_correct_html(self):
-        '''домашняя страница возвращает правильный html'''
-        response = self.client.get('/') # 1; используется вместо создания объекта HttpRequest вручную и прямого вызова функции представления; используется URL-адрес, который хотим протестировать 
-        html = response.content.decode('utf8') # извлекаем содержимое .content отклика
-        expected_html = render_to_string('lists/home.html') # преобразует шаблон в HTML-разметку
-        self.assertEqual(html, expected_html) # сравнение с тем, что возвращает представление
-
-        self.assertTemplateUsed(response, 'lists/home.html') # 3; позволяет проверить, какой шаблон использовался для вывода отклика как HTML (работает только для откликов, которые были получены тестовым клиентом).
+    def test_uses_home_template(self):
+        '''тест: используется домашний шаблон'''
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'lists/home.html') # тестовый клиент Django; имеет недостатки (т.к. есть разница между полноизолированными модульными тестами и интегрированными тестами)

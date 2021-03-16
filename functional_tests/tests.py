3946fa13d21ca4fad9006cd48c922dc281fea1d5
eldar <eleldar@mail.ru>
2021-03-16 09:44:11 +0300

    first steps for style

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -44,6 +44,30 @@ class NewVisitorTest(LiveServerTestCase):
                 time.sleep(0.5) # если мы отлавливаем исключение, то ожидаем в течение короткого периода
                                 # и в цикле делаем повторную попытку
 
+    def test_layout_and_styling(self):
+        '''тест макета и стилевого оформления'''
+        # Лена открывает домашнюю страницу
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768) # установка фиксированных значений окна
+
+        # Она замечает, что поле ввода аккуратно центрировано
+        inputbox = self.browser.find_element_by_id('id_new_item') #отыскиваем элемент input
+        self.assertAlmostEqual( # assertAlmostEqual помогает нам справиться с погрешностями округления и случайными странностями из-за полос прокрутки и т.п.
+            inputbox.location['x'] + inputbox.size['width'] / 2, # смотрим на его размер и расположение и проверяем: расположен ли он посередине страницы
+            512,
+            delta=10 # погрешность +/-10 пикселов
+        )
+
+        # Она начинает новый список и видит, что поле ввода там тоже аккуратно центрировано
+        inputbox.send_keys('тестирование')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: тестирование')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=10
+        )
 
     def test_can_start_a_list_for_one_user(self):
         '''тест: можно начать список для одного пользователя'''

493a1487b49c87facc4a7f93c637021b8e5e99ec
eldar <eleldar@mail.ru>
2021-04-05 09:42:46 +0300

    finished item validation test

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -11,16 +11,37 @@ class ItemValidationTest(FunctionalTest):
         '''тест: нельзя добавлять пустые элементы списка'''
         # Лена открывает домашнюю страницу и случайно пытается отправить
         # пустой элемент списка. Она нажимает Enter на пустом поле ввода
+        self.browser.get(self.live_server_url)
+        self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
 
         # Домашняя страница обновляется и появляется сообщение об ошибке,
         # которое говорит, что элемены списка не должны быть пустыми
+        #self.assertEqual(
+        #    self.browser.find_element_by_css_selector('.has-error').text, # указываем, что собираемся использовать класс CSS под названием .has-error, чтобы отметить текст с ошибкой
+        #    'Вы не можете вводить пустую строку!' # проверка, что наша ошибка отображает сообщение
+        #)
+        self.wait_for(lambda: self.assertEqual( # Вместо явного вызова утверждения заворачиваем его в лямбда-функцию и передаем методу wait_for, который определим в base
+            self.browser.find_element_by_css_selector('.has-error').text,
+            'Вы не можете вводить пустую строку!'
+        ))
 
         # Она пробует снова, теперь с каким-то текстом для элемента,
         # и теперь сайт не выдает ошибку
+        self.browser.find_element_by_id('id_new_item').send_keys('Купить молоко')
+        self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Купить молоко')
 
         # Далее Лена решает отправить второй пустой элемент списка
+        self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
 
         # Она получает аналогичное предупреждение на странице списка
+        self.wait_for(lambda: self.assertEqual(
+            self.browser.find_element_by_css_selector('.has-error').text,
+            'Вы не можете вводить пустую строку!'
+        ))
 
         # И она может его исправить, заполнив поле каким-то текстом
-        self.fail('НЕОБХОДИМО НАПИСАТЬ ТЕСТ')
+        self.browser.find_element_by_id('id_new_item').send_keys('Купить хлеб')
+        self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Купить молоко')
+        self.wait_for_row_in_list_table('2: Купить хлеб')

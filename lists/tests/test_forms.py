a5ff099d2b5a06b05ce922292d686125b8187d11
eldar <eleldar@mail.ru>
2021-07-04 15:37:26 +0300

    save form methods and tests for them have been deleted

--- lists/tests/test_forms.py
+++ lists/tests/test_forms.py
@@ -25,14 +25,6 @@ class ItemFormTest(TestCase):
                                           # на списки ошибок для этих полей (поле может иметь более одной ошибки).
         self.assertEqual(form.errors['text'], [EMPTY_ITEM_ERROR])
 
-    def test_form_save_handles_saving_to_a_list(self):
-        '''тест: метод save формы обрабатывает сохранение в список'''
-        list_ = List.objects.create()
-        form = ItemForm(data={'text': 'тест сохранения формой'})
-        new_item = form.save(for_list=list_)
-        self.assertEqual(new_item, Item.objects.first())
-        self.assertEqual(new_item.text, 'тест сохранения формой')
-        self.assertEqual(new_item.list, list_)
 
 class ExistingListFormTest(TestCase):
     '''тест формы элемента существуюущего списка'''
